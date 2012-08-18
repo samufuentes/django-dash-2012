b19b08705938f53d6ee30059085d6678476c19c7
Samuel Fuentes <info@samuelfuentes.com>
2012-08-18 23:55:15 +0200

    Adds the django registration urls

--- voidring/urls.py
+++ voidring/urls.py
@@ -16,4 +16,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    # Django registration
+    (r'^accounts/', include('registration.backends.default.urls')),
 )
