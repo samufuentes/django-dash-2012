10c92b17acb61061f6c00800059db7224cc85896
Alejandro Castillo <alejandro@deliveryhero.com>
2012-08-18 15:06:14 +0200

    Sets up the initial Admin

--- card/models.py
+++ card/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django_hstore import hstore
+from django.contrib import admin
 
 class Card(models.Model):
     data = hstore.DictionaryField()
@@ -7,3 +8,5 @@ class Card(models.Model):
 
     def __unicode__(self):
         return str(self.data)
+
+admin.site.register(Card)
