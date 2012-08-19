772aa5469c583c394e4811871125dd518f6d5be5
Pat <trizztz@gmail.com>
2012-08-19 17:09:19 +0200

    Point to the css in the base template

--- voidring/views.py
+++ voidring/views.py
@@ -3,7 +3,7 @@ from django.template import RequestContext
 from card.models import Card
 
 def home(request):
-    return render_to_response('home.html')
+    return render_to_response('home.html', {}, context_instance=RequestContext(request))
 
 def cards(request):
     selected = "cards"
