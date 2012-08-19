15287bc8a20337bda449f6d758161685b6cc4e2f
Pat <trizztz@gmail.com>
2012-08-19 20:57:59 +0200

    Limit the number of cards shown in list

--- voidring/views.py
+++ voidring/views.py
@@ -6,7 +6,7 @@ def home(request):
     return render_to_response('home.html', {}, context_instance=RequestContext(request))
 
 def cards(request):
-    cards = Card.objects.all
+    cards = Card.objects.all()[:10]
     selected = "cards"
     return render_to_response('cards/index.htm', {'cards': cards, 'selected': selected}, context_instance=RequestContext(request))
 
